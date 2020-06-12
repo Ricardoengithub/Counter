61f1c84af87b9f23c318e3615f3eeeb13bfea165
Ricardo Ruiz <gaanzz11@gmail.com>
2020-06-12 00:10:13 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  
+  constructor(){
+    super()
+    this.state = {
+      value: 0
+    }
+    this.increase = this.increase.bind(this)
+    this.decrease = this.decrease.bind(this)
+    this.reset = this.reset.bind(this)
+  }
+
+  increase(){
+    this.setState({
+      value: this.state.value + 1
+    })
+  }
+
+  decrease(){
+    this.setState({
+      value: this.state.value - 1
+    })
+  }
+
+  reset(){
+    this.setState({
+      value: 0
+    })
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <div id="container">
+          <h1>Counter</h1>
+          <h2>{this.state.value}</h2>
+          <button onClick={this.increase}>Increase</button>
+          <button onClick={this.reset}>Reset</button>
+          <button onClick={this.decrease}>Decrease</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
